27bc9620ec3ba1dee2d04d0d3224b4e7eefda1f9
Elia Sulimanov <eliasulimanov@gmail.com>
2020-04-17 09:15:13 +0300

    Changed map allocation
    Map allocation used to alloc the size of map pointer pre-initializtion, which was 0 bytes and this coused many read/write size problems.

--- mtm_map/map.c
+++ mtm_map/map.c
@@ -3,123 +3,104 @@
 #include "map.h"
 #include "node.h"
 
-struct Map_t {
+struct Map_t
+{
     int size;
-    Node list;
-    Node *iterator;
+    Node head;
+    Node iterator;
 };
 
-Map mapCreate() {
-    Map map = malloc(sizeof(map));
-    if(map == NULL) {
+Map mapCreate(){
+    Map map = malloc(sizeof(*map));
+    if(map == NULL){
         return NULL;
     }
-
+    //initialize empty map
     map->size = 0;
-    map->list = NULL;
-    map->iterator = NULL;
-
+    map->head = NULL;
+    map->iterator = map->head;
     return map;
 }
 
-void mapDestroy(Map map) {
-    if(map != NULL) {
-        map->iterator = &(map->list);
-        while (map->size > 0)
-        {
-            *(map->iterator) = nodeGetNext(map->list);
-            nodeDestroy(map->list);
-            map->size--;
-        }
-        if(map->list != NULL) {
-            free(map->list);
-        }
+void mapDestroy(Map map){
+    if(mapClear(map)==MAP_SUCCESS){
+        free(map->head);
         free(map);
     }
 }
 
-/*Map mapCopy(Map map) {
-    Map map_copy = malloc(sizeof(map_copy));
-    if(map_copy == NULL) {
-        return NULL;
-    }
-
-    map_copy->size = map->size;
-    //map_copy->list = malloc(sizeof(map->list)); // Check if can swap with line below
-    map_copy->list = malloc(sizeof(map_copy->list) * map_copy->size);
-    if(map_copy->list == NULL) {
-        free(map_copy);
-        return NULL;
-    }
-
-    //TODO: continue this:
-    int i = 0;
-    while (i < map_copy->size)
-    {
-        mapPut(map_copy, nodeGetKey(*(map->iterator)), nodeGetValue(*(map->iterator)));
+MapResult mapRemove(Map map, const char* key) {
+    if(map == NULL || key == NULL) {
+        return MAP_NULL_ARGUMENT;
     }
     
-}*/
+    Node temp = map->head;
+    Node previous;
 
-int mapGetSize(Map map) {
-    if(map == NULL) {
-        return -1;
+    char *temp_key = nodeGetKey(temp);
+
+    //if the item to be deleted is the first item in the list
+    if(temp != NULL && strcmp(temp_key, key) == 0) {
+        map->head = nodeGetNext(temp);
+        nodeDestroy(temp);
+        free(temp_key);
+        map->size--;
+        return MAP_SUCCESS;
     }
-    return map->size;
+
+    //if the item was not the first in the list, find the item
+    //save the location of the previous item
+    while(temp != NULL && strcmp(temp_key, key) != 0) {
+        previous = temp;
+        temp = nodeGetNext(temp);
+        temp_key = nodeGetKey(temp);
+    }
+
+    //if the item was not found
+    if(temp == NULL) {
+        free(temp_key);
+        return MAP_ITEM_DOES_NOT_EXIST;
+    }
+
+    //if the item was found, unlink it
+    nodeSetNext(previous ,nodeGetNext(temp));
+
+    nodeDestroy(temp);
+    free(temp_key);
+    map->size--;
+    return MAP_SUCCESS;
 }
 
-bool mapContains(Map map, const char* key) {
-    if(map == NULL || key == NULL) {
-        return false;
+MapResult mapClear(Map map) {
+    if(map == NULL){
+        return MAP_NULL_ARGUMENT;
+    }
+    // Find next, delete head and move next to head
+    Node temp = map->head;
+    while(temp != NULL){
+        temp = nodeGetNext(temp);
+        nodeDestroy(map->head);
+        map->head = temp;
     }
 
-    MAP_FOREACH(iterator, map) {
-        if(strcmp(iterator, key) == 0) { //TODO: Check if the iterator is really a char key and not a node.
-            return true;
-        }
-    }
-    return false;
+    nodeSetNext(map->head, NULL);
+    map->size = 0;
+    map->iterator = NULL;
+    free(temp);
+    return MAP_SUCCESS;
 }
 
-char* mapGetFirst(Map map){
-    if(map ==NULL){
-        return NULL;
-    }
-    return nodeGetKey(map->list); 
-}
-
-char* mapGetNext(Map map) {
-    if(map == NULL || map->iterator == NULL) {
-        return NULL;
-    }
-
-    *(map->iterator) = nodeGetNext(*(map->iterator)); //TODO: Check if this advances the iterator.
-
-    return nodeGetKey(*(map->iterator));
-}
-
-MapResult mapPut(Map map, const char* key, const char* data){
-    if(key == NULL || data == NULL) {
+MapResult mapPut(Map map, const char* key, const char* data) {
+    if(map == NULL || key == NULL || data == NULL) {
         return MAP_NULL_ARGUMENT;
     }
 
-    if(mapContains(map, key)) {
-        map->iterator = &(map->list);
-        for(; map->iterator; *map->iterator = nodeGetNext(*(map->iterator))){ //TODO: Check if this advances the iterator.
-            if(strcmp(nodeGetKey(*(map->iterator)), key) == 0) {
-                if(!nodeSetValue(*(map->iterator),data)) {
-                    return MAP_OUT_OF_MEMORY;
-                }
-                return MAP_SUCCESS;
-            }
-        }
-    } else {
-        Node new_node = nodeCreate(key, data, &(map->list));
-        if(new_node == NULL) {
-            return MAP_OUT_OF_MEMORY;
-        }
-        map->list = new_node; //TODO: Check in debuging if the list is changed
-        map->size++;
+    Node temp_head = map->head;
+
+    map->head = nodeCreate(key, data, &temp_head);
+    if(map->head == NULL) {
+        return MAP_OUT_OF_MEMORY;
     }
+    map->size++;
     return MAP_SUCCESS;
 }
