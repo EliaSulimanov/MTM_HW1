3376209b1b456288ba7c91bc3535b35bbaa1aaf7
Elia Sulimanov <eliasulimanov@gmail.com>
2020-04-20 13:03:40 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,19 +1,26 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "mtm_map/map.h"
+#include "election.h"
 
 int main() {
     Map map = mapCreate();
-    mapPut(map, "key", "123456789");
-    mapPut(map, "key", "matam");
-    mapGetFirst(map);
-    mapGetNext(map);
-    mapRemove(map, "key");
 
-    Map copy = mapCopy(map);
-    mapDestroy(copy);
+    mapPut(map, "area1", "40050");
+    mapPut(map, "aea2", "10");
+
+    mapPut(map, "ar", "3545464");
+    mapPut(map, "area01010100", "7");
     
+    printf("main.c -> mapGet: %s\n", mapGet(map, "area1"));
+    printf("main.c -> mapGet: %s\n", mapGet(map, "aea2"));
+    printf("main.c -> mapGet: %s\n", mapGet(map, "ar"));
+    printf("main.c -> mapGet: %s\n", mapGet(map, "area01010100"));
+    
+    char* map_str = mapToString(map);
+    //printf("%s", map_str);
+
+    free(map_str);
     mapDestroy(map);
-    return 0;
 }
