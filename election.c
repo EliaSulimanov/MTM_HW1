1ae0a685e1ab68f638452a0e09db6926cd067196
Elia Sulimanov <eliasulimanov@gmail.com>
2020-04-20 13:03:34 +0300

    Create election.c

---
+++ election.c
@@ -0,0 +1,99 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "election.h"
+#include "mtm_map/map.h"
+#include "mtm_map/node.h"
+
+struct election_t {
+    Map tribes;
+    Map areas;
+    Map votes;
+};
+
+/*
+struct Map_t
+{
+    int size;
+    Node head;
+    Node iterator;
+};
+
+struct Node_t
+{
+    char *key;
+    char *value;
+    Node next;
+};
+
+key:"tribe id"
+value:"area id;000;area id2;00;areaid3;60" int a= 00000000; => a=0; json javascript
+
+        votes
+        |
+ head:  |_"tribe_one"
+        |_"area id;000;area id2;00;areaid3;60"
+        |___
+            |_"tribe_two"
+            |_"area id;000;area id2;00;areaid3;60"
+            |_NULL
+ size: 2
+
+
+
+        tirbe_votes
+        |
+ head:  |_"area_one"
+        |_"50"
+        |___
+            |_"area_two"
+            |_"1000"
+            |_NULL
+ size: 2
+*/
+
+char* mapToString(Map map) {
+    if(map == NULL) {
+        return NULL;
+    }
+    Map copy = mapCopy(map);
+    if(copy == NULL) {
+        return NULL;
+    }
+    int size_needed_for_string = 0;
+    
+    MAP_FOREACH(key_iterator, copy) {
+        size_needed_for_string += strlen(key_iterator) + 1 //key length + ';'
+        + strlen(mapGet(copy, key_iterator)) + 1; //value length + ';'
+    }
+    
+    char *string_map = malloc(size_needed_for_string + 1); //NULL iterator
+    if(string_map == NULL) {
+        return NULL;
+    }
+    char *string_map_head = string_map;
+
+    MAP_FOREACH(key_iterator, copy){
+        strcpy(string_map, key_iterator);
+        string_map = string_map + strlen(key_iterator); // +-1
+        strcpy(string_map, "-");
+        string_map = string_map + 1;
+        
+        strcpy(string_map, mapGet(copy, key_iterator));
+        printf("get: %s\n", mapGet(copy, key_iterator));
+        printf("key: %s\n", key_iterator);
+        string_map = string_map + strlen(mapGet(copy, key_iterator)); // +-1
+        strcpy(string_map, ";");
+        string_map = string_map + 1;
+    }
+    
+    mapDestroy(copy);
+
+    return string_map_head;
+}
+
+/*static Map stringToMap(char* jason){
+    
+    //mapCreate(copy);
+    
+}*/
