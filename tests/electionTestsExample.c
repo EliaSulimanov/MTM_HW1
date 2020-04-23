8035eba37477f8604f8fc69cb5be94f8fa3d59a9
Elia Sulimanov <eliasulimanov@gmail.com>
2020-04-23 18:40:26 +0300

    Added tests for AddTribe

--- tests/electionTestsExample.c
+++ tests/electionTestsExample.c
@@ -3,7 +3,7 @@
 #include "../test_utilities.h"
 
 /*The number of tests*/
-#define NUMBER_TESTS 1
+#define NUMBER_TESTS 13
 
 bool deleteOnlyFirstArea (int area_id) {
 	return area_id == 1;
@@ -19,6 +19,56 @@ bool testElectionRemoveAreas() {
 	return true;
 }
 
+bool testElectionAddTribe() {
+    Election election = electionCreate();
+    ASSERT_TEST(electionAddTribe(election, 1, "tribe one") == ELECTION_SUCCESS);
+    ASSERT_TEST(electionAddTribe(election, 2, "other tribe") == ELECTION_SUCCESS);
+    ASSERT_TEST(electionAddTribe(election, 3, "other tribe") == ELECTION_SUCCESS);
+    electionDestroy(election);
+    return true;
+}
+
+bool testElectionAddTribeAlreadyExist() {
+    Election election = electionCreate();
+    ASSERT_TEST(electionAddTribe(election, 1, "tribe one") == ELECTION_SUCCESS);
+    ASSERT_TEST(electionAddTribe(election, 2, "other tribe") == ELECTION_SUCCESS);
+    ASSERT_TEST(electionAddTribe(election, 3, "other tribe") == ELECTION_SUCCESS);
+    ASSERT_TEST(electionAddTribe(election, 2, "nanana") == ELECTION_TRIBE_ALREADY_EXIST);
+    ASSERT_TEST(electionAddTribe(election, 3, "blue") == ELECTION_TRIBE_ALREADY_EXIST);
+    ASSERT_TEST(electionAddTribe(election, 1, "test test test") == ELECTION_TRIBE_ALREADY_EXIST);
+    ASSERT_TEST(electionAddTribe(election, 4, "work") == ELECTION_SUCCESS);
+    electionDestroy(election);
+    return true;
+}
+
+bool testElectionAddTribeInvalidId() {
+    Election election = electionCreate();
+    ASSERT_TEST(electionAddTribe(election, -1, "tribe one") == ELECTION_INVALID_ID);
+    ASSERT_TEST(electionAddTribe(election, 5-8, "work") == ELECTION_INVALID_ID);
+    electionDestroy(election);
+    return true;
+}
+
+bool testElectionAddTribeInvalidName() {
+    Election election = electionCreate();
+    ASSERT_TEST(electionAddTribe(election, 0, "Tribe") == ELECTION_INVALID_NAME);
+    ASSERT_TEST(electionAddTribe(election, 3, "w0rk") == ELECTION_INVALID_NAME);
+    ASSERT_TEST(electionAddTribe(election, 0, " ") == ELECTION_INVALID_NAME);
+    electionDestroy(election);
+    return true;
+}
+
+bool testElectionAddTribeEmptyName() {
+    Election election = electionCreate();
+    ASSERT_TEST(electionAddTribe(election, 0, "") == ELECTION_INVALID_NAME);
+    electionDestroy(election);
+    return true;
+}
+
+bool testElectionAddTribeNullElection() {
+    ASSERT_TEST(electionAddTribe(NULL, 3, "tribe one") == ELECTION_NULL_ARGUMENT);
+    return true;
+}
 /*The functions for the tests should be added here*/
 bool (*tests[]) (void) = {
                       testElectionRemoveAreas
