c4a6ef7460c6ee8df7e311b52def8dcdfc1862bc
Elia Sulimanov <eliasulimanov@gmail.com>
2020-04-24 12:13:47 +0300

    Fixed null terminator location bug

--- election.c
+++ election.c
@@ -64,8 +64,7 @@ static char* intToString(int number) {
         return NULL;
     }
     sprintf(str, "%d", number); //check if it adds null terminator at the end, or if it does the job
-    //*(str+length+1)=NULL_TERMINATOR; //BUG HERE - invalid write size
-    //yet to check if the string is terminated.
+    *(str+length) = NULL_TERMINATOR;
     return str;
 }
 
